ef07dab8ed3ec273e845acee724f603dc629987d
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air-1.local>
2016-09-10 14:03:22 -0700

    UsersModel created

--- models/user.js
+++ models/user.js
@@ -2,7 +2,14 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  name: String
+  username: {type:String, required: true},
+  languages: {type:Array, required: true}, // [{language: "French", proficiency: "Conversationalist"}, {language: German, proficiency: "Instructional"}, {language: "English", proficiency: "Native"}]
+  type: {Number , required: true}, // Translator(0) , Customer(1), Admin(9)
+  location: {type: Array, required: true}, // [Long, Lat] obtained through Geoloc
+  poIs: {Array, required: true},
+  jobReqs: {type:Schema.Types.ObjectId, ref: 'Jobs'},
+  created_at: {type: Date, default: Date.now},
+  updated_at: {type: Date, default: Date.now}
 });
 
 var User = mongoose.model('User', UserSchema);
